724b0440aaff68fda68399c7a8fa61543decb05a
Alexia Valenzuela <alexiavalenzuela@Alexias-MacBook-Air.local>
2023-05-01 19:28:28 -0700

    connected main handle bar page to server

--- server.js
+++ server.js
@@ -2,7 +2,11 @@
 const express = require('express');
 const path = require('path');
 const exphbs = require('express-handlebars');
-const hbs = exphbs.create({});
+const routes = require('./controllers');
+const helpers = require('./utils/helpers');
+const sequelize = require('./config/connection');
+
+const hbs = exphbs.create({ helpers });
 
 // Sets up the Express App
 const app = express();
@@ -13,10 +17,14 @@ app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 // Sets up the routes
-app.use(require('./controllers/home-routes'));
+// app.use(require('./controllers/home-routes'));
+
+app.use(routes);
 
 // Starts the server to begin listening
-app.listen(PORT, () => {
-  console.log('Server listening on: http://localhost:' + PORT);
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => console.log('Now listening'));
 });
