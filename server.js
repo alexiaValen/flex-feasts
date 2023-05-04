dba7304604ea0c33929e5c661a71efeedafb513c
Alexia Valenzuela <alexiavalenzuela@Alexias-MacBook-Air.local>
2023-05-03 20:09:19 -0700

    added cheerio and axios for webscraping

--- server.js
+++ server.js
@@ -5,11 +5,37 @@ const exphbs = require('express-handlebars');
 const routes = require('./controllers');
 const helpers = require('./utils/helpers');
 const sequelize = require('./config/connection');
+const axios = require('axios');
+const cheerio = require('cheerio');
 
 const hbs = exphbs.create({ helpers });
 
 // Sets up the Express App
 const app = express();
+
+//code for webscraping 
+const url = 'https://www.muscleandstrength.com/articles';
+const workouts = 'https://www.muscleandstrength.com/workout-routines';
+
+axios(url)
+  .then(response => {
+    const html = response.data
+    const $ = cheerio.load(html)
+    const articles = [] //node-short-summary
+
+    $('.node-title', html).each(function() {
+      const title = $(this).text().replace(/\n/g,'')
+      const url = $(this).find('a').attr('href')
+      articles.push({
+         title,
+          url
+      })
+    })
+    console.log(articles)
+  }).catch(err => console.log(err))
+
+
+
 const PORT = process.env.PORT || 3001;
 
 // Set Handlebars as the default template engine.
